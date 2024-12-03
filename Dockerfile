1361cf0b7180260258c8d4d0a63d7da010001379
Cole Wilson <colewilson.main@gmail.com>
2024-12-03 02:14:09 -0800

    add server and dockerfile deploy

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.13.0-alpine
+
+RUN apk update && apk upgrade && \
+    apk add --no-cache make g++ bash git openssh curl
+RUN apk update \
+    && apk add --virtual build-deps gcc python3-dev musl-dev \
+    && apk del build-deps
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+RUN python -m pip install -U --force-reinstall pip
+RUN pip install --no-cache-dir flask markdown2 --break-system-packages
+
+COPY ./server /usr/src/app
+COPY ./README.md /usr/src/app
+
+EXPOSE 9999
+EXPOSE 8080
+
+CMD ["python", "main.py"]
---
+++ server/main.py
@@ -0,0 +1,100 @@
+import random
+import socketserver
+import socket
+import markdown2
+from flask import Flask
+from threading import Thread
+
+webserver = Flask(__name__)
+
+markdown = open("./README.md").read()
+index_html = open("./index.html").read().replace("{{MARKDOWN}}", markdown2.markdown(markdown))
+
+@webserver.route("/")
+def info_page():
+    return index_html
+
+games = {}
+
+
+if "timeout" in dir(socket):
+    TimeoutError = socket.timeout
+
+class Game:
+    def __init__(self, code, server_sock):
+        self.clients = [server_sock]
+        self.code = code
+        self.players = 1
+        self.started = False
+
+
+class UnoTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        print("new connection\n\n")
+        player_index = None
+
+        initial_data = self.request.recv(1024)
+        print(f"got `{initial_data}`")
+        initial_data = initial_data.strip().strip(b"\x00").decode()
+        print(f"which is `{initial_data}`")
+        join_code = None
+        if initial_data == "new":
+            # join_code = "".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", k=6))
+            join_code = "DEBUG"
+            print("code", join_code)
+            self.request.sendall(join_code.encode())
+            games[join_code] = Game(join_code, self.request)
+            player_index = 0
+            data = self.request.recv(1024)
+            if data == b"start":
+                games[join_code].started = True
+                for client in games[join_code].clients:
+                    client.sendall(str(len(games[join_code].clients)).encode())
+
+        else:
+            # print("new", initial_data)
+            if initial_data in games:
+                join_code = initial_data
+                player_index = games[join_code].players
+                games[join_code].players += 1
+                games[join_code].clients.append(self.request)
+                self.request.sendall(f"{player_index}".encode())
+            elif games[join_code].started:
+                self.request.sendall(b"too late")
+            else:
+                self.request.sendall(b"invalid code")
+
+        self.request.settimeout(0.1)
+
+        while True:
+            try:
+                data = self.request.recv(1024)
+                print(">>", data)
+                if data == b"":
+                    return
+                for client in games[join_code].clients:
+                    if client != self.request:
+                        client.sendall(data)
+            except TimeoutError:
+                pass
+
+            # self.request.sendall(str(games[join_code].queue.qsize()).encode())
+
+            # if not games[join_code].queue.empty():
+                # self.request.sendall(games[join_code].queue.get())
+
+            # self.request.sendall(self.data.upper())
+
+
+# base code from https://docs.python.org/3/library/socketserver.html
+if __name__ == "__main__":
+    webthread = Thread(target=webserver.run, kwargs={"host":"0.0.0.0", "port":8080})
+    webthread.start()
+
+    HOST, PORT = "", 9999
+    ReuseServer = socketserver.ThreadingTCPServer
+    ReuseServer.allow_reuse_address = True
+    ReuseServer.allow_reuse_port = True
+    with ReuseServer((HOST, PORT), UnoTCPHandler) as server:
+        server.serve_forever()
+    webthread.join()
